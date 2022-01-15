850846baeed5094d2441cdec8fc390d99fa4d287
AvocadoStyle <42403145+AvocadoStyle@users.noreply.github.com>
2022-01-15 15:40:03 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,21 +2,22 @@ public class Main
 {
     public static void main(String[] args)
     {
-    Monitor m1 = new Monitor();
+        Bank b1 = new Bank(10);
 
-    Worker[] w = new Worker[20];
+        Costumer[] costumers = new Costumer[5000];
 
-    for(int i=0; i<20; i++)
-    {
-        w[i] = new Worker(i+1, m1);
+        for(int i=0; i<5000; i++)
+        {
+            costumers[i] = new Costumer(i+1, b1);
+        }
+
+        for(int i=0; i<5000; i++)
+        {
+            costumers[i].start();
+        }
+
+
+
+
     }
-
-    for(int i=0; i<20; i++)
-    {
-        w[i].start();
-    }
-
-
-    }
-
 }
